97e2cdabac6cea176b212cd143b8545fd042dd3b
Andrew Pronek <102892202+Qwaggi@users.noreply.github.com>
2022-06-21 13:00:05 -0400

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -41,30 +41,15 @@ const About = () => {
                 </h1>
 
                 <p>
-                My name is Andrew Pronek a University of Central Florida Junior. 
-                Pursuing a Major in Legal Studies with a minor in Computer Science. 
-                I enjoy the institution and the skills it lets me cultivate. 
-                My interest in law has stemmed from my interests in clubs in high school. 
-                While Computer Science stems from my constant tinkering with computers ever since I was young. 
-                The move from physical components to the actual inner workings has been the greatest revelation to experience. 
+                [REDACTED] 
                 </p>
 
                 <p>
-                For the Computer Science skills that I have been cultivating, you are looking at one. 
-                While this remains my first project that was fully completed using the React.js framework. 
-                There are so many possibilities within it, from the control of text to the 3D elements that can be created using it. 
-                Python and C are the software developing languages of choice. 
-                I resort to Python out of instinct. 
-                However, if the memory requirements are needed or complex data needs to be recorded C proves to be the workhorse on a majority of projects. 
-                Finally Rounding it out would be SQL as manually constructing a backend for software information is always a Joy to see a system work in complete unison. 
+                [REDACTED]  
                 </p>
 
                 <p>
-                My current goals, for now, are to expand on my experience in these areas even more so. 
-                I wish to take on an internship in any of these areas and learn more about the integral aspects of each. 
-                In the past, I have been a Varsity Debate member as well as both a speech competition judge and competitor. 
-                With a love for Animation and in the smallest of ways they have found themselves in my later works. 
-                Either from the mock trial or the smallest animatics, I have compiled for websites. 
+                [REDACTED] 
                 </p>
 
             </div>
